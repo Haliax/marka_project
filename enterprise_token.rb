b01d528ccaf107f2f119771ebe22e4246149aa33
Kennet <kennet@seebruecke.org>
2023-04-02 14:20:01 +0200

    v12.5.0-1

--- enterprise_token.rb
+++ enterprise_token.rb
@@ -1,17 +1,17 @@
-
 ############ REPLACE app/models/enterprise_token.rb in the source code with this file! ################
 ############ also be sure to RESTART OpenProject after replacing the file.             ################
 ############ it doesn't show that enterprise mode is enabled in the settings, but all  ################
 ############ enterprise mode features, such as KanBan boards, are enabled.             ################
+
 #-- copyright
 # OpenProject is an open source project management software.
-# Copyright (C) 2012-2023 the OpenProject GmbH
+# Copyright (C) 2012-2020 the OpenProject GmbH
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
 #
 # OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
-# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2006-2017 Jean-Philippe Lang
 # Copyright (C) 2010-2013 the ChiliProject Team
 #
 # This program is free software; you can redistribute it and/or
@@ -28,96 +28,96 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-# See COPYRIGHT and LICENSE files for more details.
+# See docs/COPYRIGHT.rdoc for more details.
 #++
 class EnterpriseToken < ApplicationRecord
-  class << self
-    def current
-      RequestStore.fetch(:current_ee_token) do
-        set_current_token
-      end
+    class << self
+        def current
+            RequestStore.fetch(:current_ee_token) do
+                set_current_token
+                end
+                end
+
+def table_exists?
+    connection.data_source_exists? self.table_name
     end
 
-    def table_exists?
-      connection.data_source_exists? table_name
+def allows_to?(action)
+    true
     end
 
-    def allows_to?(action)
-      true
+def show_banners?
+    false
     end
 
-    def show_banners?
-      false
+def set_current_token
+    token = EnterpriseToken.order(Arel.sql('created_at DESC')).first
+
+if token&.token_object
+    token
+    end
+    end
     end
 
-    def set_current_token
-      token = EnterpriseToken.order(Arel.sql('created_at DESC')).first
+validates_presence_of :encoded_token
+validate :valid_token_object
+validate :valid_domain
 
-      if token&.token_object
-        token
-      end
-    end
-  end
+before_save :unset_current_token
+before_destroy :unset_current_token
 
-  validates :encoded_token, presence: true
-  validate :valid_token_object
-  validate :valid_domain
+delegate :will_expire?,
+:subscriber,
+:mail,
+:company,
+:domain,
+:issued_at,
+:starts_at,
+:expires_at,
+:reprieve_days,
+:reprieve_days_left,
+:restrictions,
+to: :token_object
 
-  before_save :unset_current_token
-  before_destroy :unset_current_token
-
-  delegate :will_expire?,
-           :subscriber,
-           :mail,
-           :company,
-           :domain,
-           :issued_at,
-           :starts_at,
-           :expires_at,
-           :reprieve_days,
-           :reprieve_days_left,
-           :restrictions,
-           to: :token_object
-
-  def token_object
+def token_object
     load_token! unless defined?(@token_object)
     @token_object
-  end
+    end
 
-  def allows_to?(action)
+def allows_to?(action)
     true
-  end
+    end
 
-  def unset_current_token
+def unset_current_token
     # Clear current cache
     RequestStore.delete :current_ee_token
-  end
+    end
 
-  def expired?(reprieve: true)
+def expired?(reprieve: true)
     false
-  end
+    end
 
-  ##
-  # The domain is only validated for tokens from version 2.0 onwards.
-  def invalid_domain?
+##
+# The domain is only validated for tokens from version 2.0 onwards.
+def invalid_domain?
     false
-  end
+    end
 
-  private
+private
 
-  def load_token!
+def load_token!
     @token_object = OpenProject::Token.import(encoded_token)
-  rescue OpenProject::Token::ImportError => e
-    Rails.logger.error "Failed to load EE token: #{e}"
-    nil
-  end
+    rescue OpenProject::Token::ImportError => error
+        Rails.logger.error "Failed to load EE token: #{error}"
+        nil
+        end
 
-  def valid_token_object
+def valid_token_object
     errors.add(:encoded_token, :unreadable) unless load_token!
-  end
+    end
 
-  def valid_domain
+def valid_domain
     errors.add :domain, :invalid if invalid_domain?
-  end
-end
-
+    end
+    end
+    
